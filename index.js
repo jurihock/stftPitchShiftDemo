cbb66d694aa494be4213da8f7380f20197c7b844
Jürgen Hock <juergen.hock@jurihock.de>
2022-06-28 20:38:34 +0200

    Update

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
-var records = [
-];
+var data = {
+
+  "Without formant preservation":
+  [
+    {
+      "file": "https://www.dropbox.com/s/zd3wasq1pkhy2bd/voice(-q 0 -p 0.5).wav?raw=1",
+      "args": "-q 0 -p 0.5"
+    },
+    {
+      "file": "https://www.dropbox.com/s/gqq56irz8xdui1f/voice(-q 0 -p 0.75).wav?raw=1",
+      "args": "-q 0 -p 0.75"
+    },
+    {
+      "file": "https://www.dropbox.com/s/drecwnz7lu50rhi/voice(-q 0 -p 1.5).wav?raw=1",
+      "args": "-q 0 -p 1.5"
+    },
+    {
+      "file": "https://www.dropbox.com/s/6u8thi8sf8wnovz/voice(-q 0 -p 2).wav?raw=1",
+      "args": "-q 0 -p 2"
+    },
+    {
+      "file": "https://www.dropbox.com/s/q691y519bri7ti0/voice(-q 0 -p 1,1.5,2).wav?raw=1",
+      "args": "-q 0 -p 1,1.5,2"
+    },
+  ],
+
+  "With formant preservation":
+  [
+    {
+      "file": "https://www.dropbox.com/s/bve20beps4bsr3j/voice(-q 1 -p 0.5).wav?raw=1",
+      "args": "-q 1 -p 0.5"
+    },
+    {
+      "file": "https://www.dropbox.com/s/fy73zhem24kkela/voice(-q 1 -p 0.75).wav?raw=1",
+      "args": "-q 1 -p 0.75"
+    },
+    {
+      "file": "https://www.dropbox.com/s/j73gmsi5qm8hjg1/voice(-q 1 -p 1.5).wav?raw=1",
+      "args": "-q 1 -p 1.5"
+    },
+    {
+      "file": "https://www.dropbox.com/s/xkdwl4xty6v5o3i/voice(-q 1 -p 2).wav?raw=1",
+      "args": "-q 1 -p 2"
+    },
+    {
+      "file": "https://www.dropbox.com/s/v9bxtr80wmyyrv5/voice(-q 1 -p 1,1.5,2).wav?raw=1",
+      "args": "-q 1 -p 1,1.5,2"
+    },
+  ],
+
+};
